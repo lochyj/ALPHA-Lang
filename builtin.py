8f94fb7e47feb6bbc2096800366356a98e124562
Lachlan Jowett <lochyjowett1@gmail.com>
2023-04-12 12:53:20 +1000

    implemented function calls
    // Todo: update docs

---
+++ builtin.py
@@ -0,0 +1,36 @@
+import math
+
+# Math
+def sin(num):
+    num = int(num);
+    return math.sin(num);
+
+def cos(num):
+    num = int(num);
+    return math.cos(num);
+
+def tan(num):
+    num = int(num);
+    return math.sin(num)/math.cos(num);
+
+def log(num):
+    num = int(num);
+    return math.log(num);
+
+def log10(num):
+    num = int(num);
+    return math.log10(num);
+
+def sqrt(num):
+    num = int(num);
+    return math.sqrt(num);
+
+def ceil(num):
+    num = int(num);
+    return math.ceil(num);
+
+def abs(num):
+    num = int(num);
+    return math.fabs(num);
+
+# TODO: implement arc functions
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from builtin import *
 file = open("test.a");
 file_contents = file.read();
 file_lines_array = file_contents.splitlines();
@@ -6,14 +7,36 @@ types = [
     "int",
     "char",
     "str",
-    "null",
+    "arr",
     "fn",
     "//"
 ];
 
+fn_types = [
+    "null",
+    "int",
+    "char",
+    "str"
+];
+
 const = {
-    "PI": 3.1415926
-}
+    "PI": 3.1415926,
+    "E": 2.7182818
+};
+
+fn_builtins = {
+    "sin": sin,
+    "cos": cos,
+    "tan": tan,
+    "print": print,
+    "log": log,
+    "log10": log10,
+    "sqrt": sqrt,
+    "ceil": ceil,
+    "abs": abs
+};
+
+fn_user = {};
 
 output = {};
 output_index = 0;
@@ -151,6 +174,53 @@ for line in file_lines_array:
 
         output[output_index] = broken_line[1];
         output_index += 1;
+
+    elif line_return_type == "fn":
+        val = 0;
+        
+        if broken_line[2].startswith('$'):
+            if broken_line[2][1] != '-':
+                print(f"Interpreter error at: {line}, Unexpected character after stack reference.");
+                break;
+            else:
+                if output_index < int(broken_line[2][2]):
+                    print(f"Interpreter error at: {line}, Reference to stack is out of bounds. Negative index for return array.");
+                    break;
+                else:
+                    val = output[output_index - int(broken_line[2][2])];
+        
+        if broken_line[2] in const:
+            val = const[broken_line[2]];
+
+        if not broken_line[2].startswith('$'):
+            if broken_line[2] not in const:
+                val = broken_line[2];
+
+        if broken_line[1] not in fn_types:
+            if broken_line[1] in fn_builtins:
+                tmp = fn_builtins[broken_line[1]](val);
+                if tmp != None:
+                    output[output_index] = tmp;
+                    tmp = None;
+                    output_index += 1;
+            elif broken_line[1] in fn_user:
+                tmp = fn_user[broken_line[1]](val);
+                if tmp != None:
+                    output[output_index] = tmp;
+                    tmp = None;
+                    output_index += 1;
+            else:
+                print(f"Interpreter error at: {line}, Unknown function call; Function neither builtin nor user defined.");
+                break;
+        elif broken_line[1] in fn_types:
+            print(f"Interpreter error at: {line}, Function definitions aren't implemented yet.");
+            break;
+        else:
+            print(f"Interpreter error at: {line}, Missing function call or definition.");
+            break;
+        output_index += 1;
+        
+
 
 
 print(f"Output array: {output}");
