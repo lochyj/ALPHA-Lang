5f8298255ff771fd932c511b2d5dc39905155809
Lachlan Jowett <lochyjowett1@gmail.com>
2023-04-14 21:58:06 +1000

    function theory

--- src/values.py
+++ src/values.py
@@ -1,6 +1,34 @@
 # Example variable: "a": {"value": 10, "type": "int"},
 variables = {};
 
+# Example function: 
+# "add": {
+#   "inputs": {
+#       "x": "int", 
+#       "y": "int"
+#   }, 
+#   "function_string": {
+#       "return": {"input": "x", "operation": "+", "input": "y"}
+#   }
+# }
+
+# Example function: 
+# "increase_money": {
+#   "inputs": {
+#       "increase_amount": "int"
+#   }, 
+#   "function_string": {
+#       0: {"variable": "money", "operation": "+=", "input": "increase_amount"}
+#   }
+# }
+
+# 0: is the line of the function
+# "return": is indicating that the function will return this value
+
+# This should be used for variable values when the variable is defined in evaluate(string: str)
+
+functions = {};
+
 # characters that cannot be in function, variable or datatype identifier. There is a special case for functions as they require () to define inputs.
 special_characters = [
     "[",
